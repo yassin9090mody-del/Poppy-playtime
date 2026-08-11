5b44d6340c3600535fb20a07361fba2b386d368b
yassin9090mody-del <yassin9090mody@gmail.com>
2026-08-12 02:12:00 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,40 +2,43 @@
 #include <pspdisplay.h>
 #include <pspctrl.h>
 #include <pspgu.h>
-#include <stdlib.h>
+#include <pspgum.h>
 
-PSP_MODULE_INFO("Shadow Runner", 0, 1, 0);
+PSP_MODULE_INFO("Vector PSP", 0, 1, 0);
 PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_USER);
 
 #define SCREEN_W 480
 #define SCREEN_H 272
 
-static unsigned int __attribute__((aligned(16))) list[262144];
-
-typedef struct
-{
+typedef struct {
+    unsigned int color;
     float x;
     float y;
-    float w;
-    float h;
-    unsigned int color;
-} RectVertex;
+    float z;
+} Vertex;
 
-static void drawRect(float x, float y, float w, float h, unsigned int color)
+static unsigned int __attribute__((aligned(16))) list[262144];
+
+/* رسم مستطيل */
+static void drawRect(
+    float x,
+    float y,
+    float w,
+    float h,
+    unsigned int color
+)
 {
-    RectVertex *v = (RectVertex *)sceGuGetMemory(sizeof(RectVertex) * 2);
+    Vertex *v = (Vertex *)sceGuGetMemory(2 * sizeof(Vertex));
 
+    v[0].color = color;
     v[0].x = x;
     v[0].y = y;
-    v[0].w = 0;
-    v[0].h = 0;
-    v[0].color = color;
+    v[0].z = 0.0f;
 
+    v[1].color = color;
     v[1].x = x + w;
     v[1].y = y + h;
-    v[1].w = 0;
-    v[1].h = 0;
-    v[1].color = color;
+    v[1].z = 0.0f;
 
     sceGuDrawArray(
         GU_SPRITES,
@@ -48,46 +51,13 @@ static void drawRect(float x, float y, float w, float h, unsigned int color)
     );
 }
 
-static void drawPlayer(float x, float y)
-{
-    /* الرأس */
-    drawRect(x + 6, y - 10, 14, 14, 0xFF050505);
-
-    /* الجسم */
-    drawRect(x + 8, y + 4, 10, 25, 0xFF050505);
-
-    /* الذراع */
-    drawRect(x + 1, y + 8, 10, 5, 0xFF050505);
-
-    /* الرجلان */
-    drawRect(x + 6, y + 27, 6, 18, 0xFF050505);
-    drawRect(x + 14, y + 27, 6, 18, 0xFF050505);
-}
-
-static void drawChaser(float x, float y)
-{
-    /* رأس المطارد */
-    drawRect(x + 6, y - 10, 14, 14, 0xFF303030);
-
-    /* جسم المطارد */
-    drawRect(x + 8, y + 4, 10, 25, 0xFF303030);
-
-    /* الذراعان */
-    drawRect(x + 1, y + 8, 10, 5, 0xFF303030);
-    drawRect(x + 17, y + 8, 10, 5, 0xFF303030);
-
-    /* الرجلان */
-    drawRect(x + 6, y + 27, 6, 18, 0xFF303030);
-    drawRect(x + 14, y + 27, 6, 18, 0xFF303030);
-}
-
 int main(void)
 {
-    SceCtrlData pad;
-
+    /* التحكم */
     sceCtrlSetSamplingCycle(0);
-    sceCtrlSetSamplingMode(PSP_CTRL_MODE_DIGITAL);
+    sceCtrlSetSamplingMode(PSP_CTRL_MODE_ANALOG);
 
+    /* تشغيل GU */
     sceGuInit();
 
     sceGuStart(GU_DIRECT, list);
@@ -110,6 +80,9 @@ int main(void)
         512
     );
 
+    /*
+     * إعداد الشاشة 480x272
+     */
     sceGuOffset(
         2048 - SCREEN_W / 2,
         2048 - SCREEN_H / 2
@@ -122,7 +95,13 @@ int main(void)
         SCREEN_H
     );
 
-    sceGuScissor(0, 0, SCREEN_W, SCREEN_H);
+    sceGuScissor(
+        0,
+        0,
+        SCREEN_W,
+        SCREEN_H
+    );
+
     sceGuEnable(GU_SCISSOR_TEST);
 
     sceGuDisable(GU_DEPTH_TEST);
@@ -135,318 +114,133 @@ int main(void)
     sceGuDisplay(GU_TRUE);
 
     /* اللاعب */
-    float playerX = 120.0f;
-    float playerY = 190.0f;
+    float playerX = 70.0f;
+    float playerY = 200.0f;
 
-    float playerVelocityY = 0.0f;
-    int playerGrounded = 1;
+    float velocityY = 0.0f;
+    int onGround = 1;
 
-    /* المطارد */
-    float chaserX = 35.0f;
-    float chaserY = 190.0f;
-
-    /* العوائق */
-    float obstacle1X = 380.0f;
-    float obstacle2X = 650.0f;
-
-    int gameOver = 0;
-    int won = 0;
-
-    int distance = 0;
+    /* العائق */
+    float obstacleX = 350.0f;
+    float obstacleY = 215.0f;
 
     while (1)
     {
+        SceCtrlData pad;
+
         sceCtrlReadBufferPositive(&pad, 1);
 
         /*
-         * إعادة اللعبة
-         * START
+         * القفز
          */
-        if (gameOver || won)
+        if ((pad.Buttons & PSP_CTRL_CROSS) && onGround)
         {
-            if (pad.Buttons & PSP_CTRL_START)
-            {
-                playerX = 120.0f;
-                playerY = 190.0f;
-                playerVelocityY = 0.0f;
-                playerGrounded = 1;
-
-                chaserX = 35.0f;
-                chaserY = 190.0f;
-
-                obstacle1X = 380.0f;
-                obstacle2X = 650.0f;
-
-                distance = 0;
-
-                gameOver = 0;
-                won = 0;
-            }
-        }
-        else
-        {
-            /*
-             * القفز
-             * X
-             */
-            if ((pad.Buttons & PSP_CTRL_CROSS) &&
-                playerGrounded)
-            {
-                playerVelocityY = -10.5f;
-                playerGrounded = 0;
-            }
-
-            /*
-             * الجاذبية
-             */
-            playerVelocityY += 0.5f;
-            playerY += playerVelocityY;
-
-            /*
-             * الأرض
-             */
-            if (playerY >= 190.0f)
-            {
-                playerY = 190.0f;
-                playerVelocityY = 0.0f;
-                playerGrounded = 1;
-            }
-
-            /*
-             * سرعة العالم
-             */
-            obstacle1X -= 4.0f;
-            obstacle2X -= 4.0f;
-
-            /*
-             * إعادة العوائق
-             */
-            if (obstacle1X < -40.0f)
-            {
-                obstacle1X = 520.0f + rand() % 100;
-                distance++;
-            }
-
-            if (obstacle2X < -40.0f)
-            {
-                obstacle2X = 650.0f + rand() % 150;
-                distance++;
-            }
-
-            /*
-             * المطارد يجري خلف اللاعب
-             */
-            if (chaserX < playerX - 55.0f)
-            {
-                chaserX += 1.0f;
-            }
-
-            /*
-             * لو المطارد قرب جدًا
-             */
-            if (chaserX + 25.0f >= playerX)
-            {
-                gameOver = 1;
-            }
-
-            /*
-             * اصطدام اللاعب بالعائق الأول
-             */
-            if (playerX + 20.0f > obstacle1X &&
-                playerX < obstacle1X + 28.0f &&
-                playerY + 45.0f > 210.0f)
-            {
-                gameOver = 1;
-            }
-
-            /*
-             * اصطدام اللاعب بالعائق الثاني
-             */
-            if (playerX + 20.0f > obstacle2X &&
-                playerX < obstacle2X + 28.0f &&
-                playerY + 45.0f > 200.0f)
-            {
-                gameOver = 1;
-            }
-
-            /*
-             * الوصول لنهاية المرحلة
-             */
-            if (distance >= 20)
-            {
-                won = 1;
-            }
+            velocityY = -9.5f;
+            onGround = 0;
         }
 
+        /*
+         * الجاذبية
+         */
+        velocityY += 0.45f;
+        playerY += velocityY;
+
+        /*
+         * الأرض
+         */
+        if (playerY >= 200.0f)
+        {
+            playerY = 200.0f;
+            velocityY = 0.0f;
+            onGround = 1;
+        }
+
+        /*
+         * تحريك العائق
+         */
+        obstacleX -= 3.0f;
+
+        if (obstacleX < -40.0f)
+        {
+            obstacleX = 520.0f;
+        }
+
+        /*
+         * الاصطدام
+         */
+        if (
+            playerX + 20.0f > obstacleX &&
+            playerX < obstacleX + 25.0f &&
+            playerY + 40.0f > obstacleY
+        )
+        {
+            playerX = 70.0f;
+            obstacleX = 350.0f;
+        }
+
+        /*
+         * بداية الرسم
+         */
         sceGuStart(GU_DIRECT, list);
 
         /*
          * الخلفية
          */
-        sceGuClearColor(0xFFE8E8E8);
+        sceGuClearColor(0xFFF2F2F2);
         sceGuClear(GU_COLOR_BUFFER_BIT);
-
-        /*
-         * مباني الخلفية
-         */
-        drawRect(0, 120, 55, 115, 0xFFD0D0D0);
-        drawRect(60, 90, 70, 145, 0xFFC8C8C8);
-        drawRect(135, 135, 60, 100, 0xFFD5D5D5);
-        drawRect(200, 105, 75, 130, 0xFFCCCCCC);
-        drawRect(280, 135, 65, 100, 0xFFD2D2D2);
-        drawRect(350, 95, 70, 140, 0xFFC5C5C5);
-        drawRect(425, 125, 55, 110, 0xFFD0D0D0);
 
         /*
          * الأرض
          */
         drawRect(
-            0,
-            235,
-            480,
-            37,
-            0xFF181818
+            0.0f,
+            240.0f,
+            480.0f,
+            32.0f,
+            0xFF202020
         );
 
         /*
-         * خط الأرض
+         * العائق
          */
         drawRect(
-            0,
-            231,
-            480,
-            4,
+            obstacleX,
+            obstacleY,
+            25.0f,
+            25.0f,
+            0xFF202020
+        );
+
+        /*
+         * جسم اللاعب
+         */
+        drawRect(
+            playerX,
+            playerY,
+            20.0f,
+            40.0f,
             0xFF000000
         );
 
         /*
-         * العوائق
+         * رأس اللاعب
          */
         drawRect(
-            obstacle1X,
-            210,
-            28,
-            25,
-            0xFF101010
-        );
-
-        drawRect(
-            obstacle2X,
-            200,
-            28,
-            35,
-            0xFF101010
+            playerX + 2.0f,
+            playerY - 10.0f,
+            16.0f,
+            12.0f,
+            0xFF000000
         );
 
         /*
-         * المطارد
+         * إنهاء الرسم
          */
-        drawChaser(chaserX, chaserY);
-
-        /*
-         * اللاعب
-         */
-        drawPlayer(playerX, playerY);
-
-        /*
-         * شريط التقدم
-         */
-        drawRect(
-            20,
-            18,
-            160,
-            6,
-            0xFFAAAAAA
-        );
-
-        drawRect(
-            20,
-            18,
-            (distance % 21) * 8,
-            6,
-            0xFF111111
-        );
-
-        /*
-         * Game Over
-         */
-        if (gameOver)
-        {
-            drawRect(
-                80,
-                85,
-                320,
-                100,
-                0xFFEEEEEE
-            );
-
-            drawRect(
-                120,
-                105,
-                240,
-                10,
-                0xFF111111
-            );
-
-            drawRect(
-                150,
-                130,
-                180,
-                8,
-                0xFF555555
-            );
-
-            drawRect(
-                180,
-                155,
-                120,
-                6,
-                0xFF777777
-            );
-        }
-
-        /*
-         * الفوز
-         */
-        if (won)
-        {
-            drawRect(
-                80,
-                85,
-                320,
-                100,
-                0xFFEAEAEA
-            );
-
-            drawRect(
-                120,
-                105,
-                240,
-                10,
-                0xFF111111
-            );
-
-            drawRect(
-                145,
-                130,
-                190,
-                8,
-                0xFF555555
-            );
-
-            drawRect(
-                170,
-                155,
-                140,
-                6,
-                0xFF777777
-            );
-        }
-
         sceGuFinish();
         sceGuSync(0, 0);
 
         sceDisplayWaitVblankStart();
+
         sceGuSwapBuffers();
     }
 
